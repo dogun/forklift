2be2bd34c3242fe79fa7d772cd511e19bb50e0ae
dogun <yuexuqiang@gmail.com>
2023-12-03 16:32:55 +0800

    call forklift

--- software/master/remote_run.php
+++ software/master/remote_run.php
@@ -7,9 +7,25 @@ $macro = @$_GET['macro'];
 $host = @$_SERVER['REMOTE_ADDR'];
 $printer = @$_GET['printer'];
 
-echo "$printer $action $macro $host";
+echo "$printer $action $macro $host \n";
 
-$pr = $mysqli->query("select * from printers where name='".$mysqli->real_escape_string($printer)."'");
-$r = $pr->fetch_assoc();
-print_r($r);
+if ($action == 'call_forklift') {
+	$pr = $mysqli->query("select * from printers where name='".$mysqli->real_escape_string($printer)."'");
+	$r = $pr->fetch_assoc();
+	if (!$r) {
+		die('ERROR: PRINTER NOT FOUND');
+	}
 
+	$forklift_id = $r['forklift_id'];
+	$fl = $mysqli->query("select * from forklifts where id=$forklift_id");
+	$r = $fl->fetch_assoc();
+	if (!$r) {
+		die('ERROR: FORKLIFT NOT FOUND');
+	}
+
+	$host = $r['host'];
+	$port = $r['port'];
+	$url = "http://$host:$port/printer/gcode/script?script=SHOULIAO";
+	echo file_get_contents($url);
+
+}
