1f7e2b36eb6c67ec7124ee6bb3fc3bfd54bb8695
dogun <yuexuqiang@gmail.com>
2023-12-26 00:33:31 +0800

    update

--- software/master/remote_run.php
+++ software/master/remote_run.php
@@ -22,12 +22,33 @@ if ($action == 'call_forklift') {
 	if (!$r) {
 		die('ERROR: FORKLIFT NOT FOUND');
 	}
-
 	$host = $r['host'];
 	$port = $r['port'];
-	$url = "http://$host:$port/printer/gcode/script?script=SHOULIAO";
-	$sql = "insert into action_queue (action, content, gmt_created, gmt_modified, status) values('call_forklift', '$url', ".time().", 0, 0)";
-	$r = $mysqli->query($sql) or die('INSERT QUEUE ERROR');
-	echo "QUEUED";
 
+	if (false) {
+		$url = "http://$host:$port/printer/gcode/script?script=SHOULIAO";
+		$sql = "insert into action_queue (action, content, gmt_created, gmt_modified, status) values('call_forklift', '$url', ".time().", 0, 0)";
+		$r = $mysqli->query($sql) or die('INSERT QUEUE ERROR');
+		echo "QUEUED";
+	} else {
+		$url = "http://$host:$port/printer/gcode/script?script=$macro";
+		echo $url."\n";
+		$res = file_get_contents($url);
+		echo $res;
+	}
+
+}
+if ($action == 'call_printer') {
+	$printer = 'NO.2';
+	$pr = $mysqli->query("select * from printers where name='".$mysqli->real_escape_string($printer)."'");
+	$r = $pr->fetch_assoc();
+	if (!$r) {
+		die('ERROR: PRINTER NOT FOUND');
+	}
+	$host = $r['host'];
+	$port = $r['port'];
+	$url = "http://$host:$port/printer/gcode/script?script=$macro";
+	echo $url."\n";
+	$res = file_get_contents($url);
+	echo $res;
 }
