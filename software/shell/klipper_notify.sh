cbd9cebb263438a8a87f2039f4ad8c3bdaea520b
dogun <yuexuqiang@gmail.com>
2024-04-15 14:40:13 +0800

    add 1462000287@qq.com

--- software/shell/klipper_notify.sh
+++ software/shell/klipper_notify.sh
@@ -6,4 +6,5 @@ action=$1
 pname=$2
 gcode=$3
 
-cat "/home/dogun/notify/${action}.eml" | sed "s/{time}/$time/" | sed "s/{print_name}/ $pname /" | sed "s/{gcode_name}/ $gcode /" | ssmtp 2246761@qq.com
+cat "/home/dogun/shell/notify/${action}.eml" | sed "s/{time}/$time/" | sed "s/{print_name}/ $pname /" | sed "s/{gcode_name}/ $gcode /" | ssmtp 2246761@qq.com
+cat "/home/dogun/shell/notify/${action}.eml" | sed "s/{time}/$time/" | sed "s/{print_name}/ $pname /" | sed "s/{gcode_name}/ $gcode /" | ssmtp 1462000287@qq.com
