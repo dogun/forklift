98c07168b79db7a542490b4982e2c405be82f936
dogun <yuexuqiang@gmail.com>
2024-08-15 23:04:52 +0800

    u

--- software/master/tvoc_show.php
+++ software/master/tvoc_show.php
@@ -2,23 +2,57 @@
 include('inc.php');
 $stime = intval(@$_GET['stime']);
 $etime = intval(@$_GET['etime']);
-$uid = $mysqli->real_escape_string(@$_GET['uid']);
 
-$q = "select uid, tvoc, co2, f, time from tvoc_log where uid='$uid'";
-if ($stime > 0) $q .= " and time >= CURRENT_TIMESTAMP() - $stime";
-if ($etime > 0) $q .= " and time <= CURRENT_TIMESTAMP() - $etime";
-$q .= " order by id desc";
+$dv = 60;
+
+$s = time() - $stime;
+$e = time() - $etime;
+
+$q = "select uid, tvoc, co2, f, UNIX_TIMESTAMP(time) as t from tvoc_log where ";
+$q .= " time >= FROM_UNIXTIME($s)";
+$q .= " and time <= FROM_UNIXTIME($e)";
+$q .= " order by id asc";
 
 $r = $mysqli->query($q) or die('');
-$arr = array();
+$a1 = array();
+$a2 = array();
 while (($list = $r->fetch_assoc())) {
-	if ($uid == 'work1') $tvoc = $list['tvoc'] - 230;
-	if ($uid == 'work2') $tvoc = $list['co2'];
-	$arr[] = array($list['time'], $tvoc);
+	$time = intval($list['t'] / $dv);
+	$uid = $list['uid'];
+	if ($uid == 'work1') {
+		$tvoc = $list['tvoc'] - 230;
+		$a1[$time] = $tvoc;
+	}
+	if ($uid == 'work2') {
+		$tvoc = $list['co2'] * 3;
+		$a2[$time] = $tvoc;
+	}
 }
 
-foreach ($arr as $v) {
-	echo $v[0].' '.$v[1].'<br />';
-	print_r($v[0]);
+?>
+<html>
+<body>
+<table border="0">
+<?php
+$s = intval($s / $dv);
+$e = intval($e / $dv);
+$l1 = 0;
+$l2 = 0;
+for ($i = $s; $i < $e; ++$i) {
+	$tvoc1 = intval(@$a1[$i]);
+	$tvoc2 = intval(@$a2[$i]);
+	if ($tvoc1 == 0) $tvoc1 = $l1;
+	if ($tvoc2 == 0) $tvoc2 = $l2;
+	$l1 = $tvoc1;
+	$l2 = $tvoc2;
+	echo '<tr>';
+	echo '<td>'.date('Y-m-d H:i:s', $i*$dv).'</td>';
+	echo '<td>';
+	echo '<table border="0" style="background-color:#F00"><tr><td style="width:'.$tvoc1.'px">'.$tvoc1.'</td></tr></table>';
+	echo '<table border="0" style="background-color:#000"><tr><td style="width:'.$tvoc2.'px;color:#FFF">'.$tvoc2.'</td></tr></table>';
+	echo '</td>';
+	echo '</tr>';
 }
-
+?>
+</table>
+</body></html>
