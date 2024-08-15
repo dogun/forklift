72e6ae43abddf15286ac78cfcb4c754eda945534
dogun <yuexuqiang@gmail.com>
2024-08-15 16:02:06 +0800

    u

--- software/master/tvoc_show.php
+++ software/master/tvoc_show.php
@@ -2,10 +2,15 @@
 include('forklift/inc.php');
 $stime = intval($_GET['stime']);
 $etime = intval($_GET['etime']);
-$uid = $_GET['uid'];
+$uid = $mysqli->real_escape_string($_GET['uid']);
 
-$q = "select uid, tvoc, co2, f, time from tvoc_log where uid=''";
+$q = "select uid, tvoc, co2, f, time from tvoc_log where uid='$uid'";
 if ($stime > 0) $q .= " and time >= CURRENT_TIMESTAMP() - $stime";
 if ($etime > 0) $q .= " and time <= CURRENT_TIMESTAMP() - $etime";
+$q .= " order by id desc";
 
-echo $q;
+$r = mysqli->query($q) or die('');
+$list = $r->fetch_assoc();
+for ($list as $val) {
+	echo $val['uid'].' '.$val['tvoc'].'<br />';
+}
