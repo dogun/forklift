98c219a4c046fcbef44d675209c13a2da8ebe070
dogun <yuexuqiang@gmail.com>
2025-05-04 15:03:47 +0800

    u

--- software/master/pull.php
+++ software/master/pull.php
@@ -1,7 +1,12 @@
 <?php
 $dir = __DIR__;
-$cmd = "cd $dir; git pull";
-echo $cmd;
-exec($cmd, $res);
-print_r($res);
+$repoPath = $dir;
+$output = [];
+$returnCode = 0;
+exec("git -C $repoPath pull origin master", $output, $returnCode);
+if ($returnCode === 0) {
+    echo "Git pull 成功";
+} else {
+    echo "Git pull 失败，错误信息：" . implode("\n", $output);
+}
 ?>
