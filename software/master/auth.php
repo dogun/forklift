19a5b2f4926d3e07ec6dfdaeaba7c47aa96bd6f9
dogun <yuexuqiang@gmail.com>
2025-05-05 02:20:48 +0800

    u

--- software/master/auth.php
+++ software/master/auth.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
 $user_id = $_SEESION['user_id'];
 $user = _query_user($user_id);
 if (!$user || $user['status'] != USER_STATUS::NORMAL->value) {
-    header("Location: login.php?url=".urlencode($_SERVER['REQUEST_URI']));
+    header("Location: login.php?user=error&url=".urlencode($_SERVER['REQUEST_URI']));
     exit();
 }
 ?>
