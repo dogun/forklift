f2f763f744e7a029f0adf94f94ec6ac41101cf7a
dogun <yuexuqiang@gmail.com>
2025-05-04 13:49:42 +0800

    u

--- software/master/queue_log.php
+++ software/master/queue_log.php
@@ -1,5 +1,17 @@
+<html lang="zh-CN">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>队列日志</title>
+</head>
+<body>
 <?php
 $res = '';
 exec('tail collect.log -n 100', $res);
-print_r($res);
+foreach ($res as $l) {
+	echo $l.'<br />';
+}
 ?>
+</body>
+</html>
