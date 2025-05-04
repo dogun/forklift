64e9ed4016d021d112cdf8d57ffa2ea152b65019
dogun <yuexuqiang@gmail.com>
2025-05-05 02:18:11 +0800

    u

--- software/master/auth.php
+++ software/master/auth.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
-    header("Location: login.php?url=".urlencode($__FILE__));
+    header("Location: login.php?url=".urlencode($_SERVER['REQUEST_URI']));
     exit();
 }
 $user_id = $_SEESION['user_id'];
 $user = _query_user($user_id);
 if (!$user || $user['status'] != USER_STATUS::NORMAL->value) {
-    header("Location: login.php?url=".urlencode($__FILE__));
+    header("Location: login.php?url=".urlencode($_SERVER['REQUEST_URI']));
     exit();
 }
 ?>
