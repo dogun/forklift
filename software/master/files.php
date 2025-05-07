86966f88bccfcc8417d659ff77a9563b6e21c240
dogun <yuexuqiang@gmail.com>
2025-05-07 22:51:35 +0800

    u

--- software/master/files.php
+++ software/master/files.php
@@ -7,7 +7,7 @@ if ($file && $file['size'] > 0) {
 	$file_name = $file['name'];
 	$size = $file['size'];
 	$path = $file['tmp_name'];
-	$id = _insert_task_file($file_name, $size, $user_id, 'ABS', 'BLACK');
+	$id = _insert_print_file($file_name, $size, $user_id, 'ABS', 'BLACK');
 	$fname = $FILE_PATH.$id;
 	move_uploaded_file($path, $fname);
 	echo $fname;
