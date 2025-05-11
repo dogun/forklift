aac5fed6fafed86ebda52458cda309c69825b4be
dogun <yuexuqiang@gmail.com>
2025-05-11 23:13:43 +0800

    u

--- software/master/inc.php
+++ software/master/inc.php
@@ -394,7 +394,7 @@ function _update_print_files_queue_status($f_id, $status) {
 	global $mysqli;
 	$f_id = intval($f_id);
 	$status = PRINT_FILES_STATUS::from($status)->value;
-	$mysql_query("update print_files_queue set status='$status', modified=CURRENT_TIMESTAMP() where id=$fid");
+	$mysqli->query("update print_files_queue set status='$status', modified=CURRENT_TIMESTAMP() where id=$fid");
 }
 
 #test
