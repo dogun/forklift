2a5f38e02f56312ad044fbbc5474969e17f6154a
dogun <yuexuqiang@gmail.com>
2025-06-06 01:57:05 +0800

    u

--- software/shell/remote_run.sh
+++ software/shell/remote_run.sh
@@ -5,7 +5,7 @@ action=$2
 macro=$3
 
 
-url="http://192.168.7.1/forklift/remote_run.php?printer=${name}&action=${action}&macro=${macro}"
+url="http://192.168.7.1/forklift/remote_run.php?macro=${macro}&printer=${name}&action=${action}"
 echo $url
 curl $url
 
